c960d4a36da87e5d0fb380aff576fccf6bd28f55
Dan Lo <nflwwe71@gmail.com>
2020-04-30 02:04:04 -0400

    Draftfed Version of show App. Fixed the if else. Added some CSS

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 let user = prompt("What is your name?");
 let color = prompt("What is your favorite color?");
 
-function lover(color)
-{
+function lover(color){
 
-    if(color == "blue")
-{
+
+    if(color === "blue"){
+
     alert("Thats my favorite color");
-}
-if (color == "gold") 
-{
+    
+    }
+    
+
+    else if (color === "gold"){
+
     alert("You have rich tendencies, like me.");
-    // break;
-}
- 
-else
-{
-    alert("thats a trash ass color and it aint compatible!");
-}
+    
+    }
 
+    else if(color === "green"){
+       alert("I see why you chose that color, you like capital. #Mr.Krabs Tendencies")
+    }
 
+    else{
 
-document.getElementById("love").innerHTML=color;
-}
+    alert("thats a trash color and it is not compatible!");
+    }
+} 
 
 lover(color)
+
+document.getElementById("love").innerHTML=color;
+
+
+
